dd4f6d1aebbda084204a4c1d880a9d3531e86b80
Moises Quintero Morales <132148902+MQM360@users.noreply.github.com>
2023-09-30 07:26:22 -0400

    Update mario2.c

--- Beginner_Mario/mario2.c
+++ Beginner_Mario/mario2.c
@@ -1,1 +1,39 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int height;
+
+    // Check for Height
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // iterate through pyramid (rows and col.)
+    for (int i = 0; i < height; i++) // ROW or X
+    {
+        // Left Side: Spaces & Hash
+        for (int j = i + 1; j < height; j++) // COL or Y1
+        {
+            printf(" ");
+        }
+        for (int k = height - i; k < height + 1; k++) // COL or Y2
+        {
+            printf("#");
+        }
+
+        // Right Side: Spaces & Hash
+        for (int l = height - 2; l < height; l++) // COL or Y1
+        {
+            printf(" ");
+        }
+        for (int m = height - i; m < height + 1; m++) // COL or Y2
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
